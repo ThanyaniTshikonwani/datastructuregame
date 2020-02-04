a6d602cea907d75831a12616cb02c3fb6e178efc
Thanyani Tshikonwani <thanyanitshikonwani@umuzi.org>
2020-02-04 19:12:37 +0200

    Final

--- src/main/java/DataStructure.java
+++ src/main/java/DataStructure.java
@@ -1,6 +1,3 @@
-import java.util.ArrayList;
-import java.util.Arrays;
-
 public class DataStructure {
 
     int w ;
@@ -28,21 +25,86 @@ public class DataStructure {
         }
     }
 
-    public void deadCell(int x,int y){
-        this.board[x][y] =0;
-    }
-    public void aliveCell(int x,int y){
-        this.board[x][y] =1;
+    public void deadCell(int x , int y) {
+        this.board[x][y] = 0;
     }
 
+    public void aliveCell(int x , int y) {
+        this.board[x][y] = 1;
+    }
+
+    public int countNeighbours(int x , int y) {
+        int count = 0;
+        count += getState ( x - 1 , y-1 );
+        count += getState ( x  , y );
+        count += getState ( x - 1 , y-1 );
+
+        count += getState ( x + 1 , y );
+        count += getState ( x - 1 , y );
+
+        count += getState ( x -1 , y +1);
+        count += getState ( x  , y +1);
+        count += getState ( x + 1 , y+1 );
+        return count;
+    }
+
+    public int getState(int x , int y) {
+        if (x < 0 || x >= w) {
+            return 0;
+        }
+
+        if (y < 0 || y >= h) {
+            return 0;
+        }
+        return this.board[x][y];
+    }
+
+    public void generation() {
+
+        int[][] newBoard = new int[w][h];
+        for (int y = 0; y < h; y++) {
+            for (int x = 0; x < w; x++) {
+                int aliveNeighbours = countNeighbours ( x , y );
+
+                if (this.board[x][y] == 1) {
+                    if (aliveNeighbours < 2) {
+                        newBoard[x][y] = 0;
+                    } else if (aliveNeighbours == 2 || aliveNeighbours == 3) {
+                        newBoard[x][y] = 1;
+                    } else if (aliveNeighbours > 3) {
+                        newBoard[x][y] = 0;
+                    }
+                } else {
+                    if (aliveNeighbours == 3) {
+                        newBoard[x][y] = 1;
+                    }
+                }
+            }
+        }
+        this.board = newBoard;
+    }
 
     public static void main(String[] args) {
-        DataStructure dataStructure = new DataStructure ( 6,3 );
+        DataStructure dataStructure = new DataStructure ( 10 , 10 );
 
-        System.out.println ("  Input");
-        dataStructure.aliveCell ( 2,0 );
-        dataStructure.aliveCell ( 2,1 );
-        dataStructure.aliveCell ( 3,1 );
+        System.out.println ( "  Input" );
+
+        dataStructure.aliveCell ( 3, 0 );
+        dataStructure.deadCell ( 0,0 );
+        dataStructure.deadCell ( 1,0 );
+        dataStructure.deadCell ( 2,0 );
+        dataStructure.aliveCell ( 2 , 1 );
+        dataStructure.aliveCell ( 3 , 1 );
+        dataStructure.aliveCell ( 4 , 1 );
+//        dataStructure.deadCell ( 0,3 );
+//        dataStructure.deadCell ( 1,3 );
+//        dataStructure.deadCell ( 2,3 );
+
+        dataStructure.showBoard ();
+
+        System.out.println ( "  generation " );
+        dataStructure.generation ();
+
         dataStructure.showBoard ();
     }
 }
